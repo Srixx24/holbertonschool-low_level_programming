3c9a4ff150fffbf0cc0a0e3ea4f28808f892acf0
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-09-20 19:28:26 -0700

    first push

---
+++ hello_world/5-printf.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/*
+ * File: 5-printf.c
+ * By: Jackie L
+ * printf function
+ */
+int printf (void)
+{
+	/*String Start*/
+	printf ("with proper grammar, but the outcome is a piece of art,");
+	/*Main fuction using printf
+	 * with proper grammar, but the outcome is a piece of art,
+	 *
+	 * Return: 0
+	 */
+	return (0)
+}
