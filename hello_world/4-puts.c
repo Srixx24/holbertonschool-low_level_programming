83e50c687532e3147c1a8af9d002a45f3581d7d9
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-09-20 18:49:42 -0700

    checking

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-#include <string.h>
 
 /**main*/
 int main(void)
 {
 	/**String start*/
-	puts = "\"Programming is like building a multilingual puzzle";
+	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
 }
