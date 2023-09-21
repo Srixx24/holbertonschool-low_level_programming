5f5b54ffe63b5847acb4f02135fdbbe70832530e
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-09-20 19:52:07 -0700

    first push

---
+++ hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdoi.h>
+/*File: 6-size.c
+ * By: Jackie L
+ */
+int main(void)
+{
+	/*
+	 * Main - Printing size of types in bytes
+	 */
+	printf("Size of a char: 1 byte(s)");
+	printf("Size of an int: 4 byte(s)");
+	printf("Size of a long int: 4 byte(s)");
+	printf("Size of a long long int: 8 byte(s)");
+	printf("Size of a float: 4 byte(s)");
+
+	return (0);
+}
