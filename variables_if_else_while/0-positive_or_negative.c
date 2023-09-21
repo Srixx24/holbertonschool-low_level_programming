f89ff96857edc04bc9bedd6c5459340f24bfc925
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-09-21 09:32:12 -0700

    first push

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -7,22 +7,22 @@
  *
  * Return: 0
  *
- * */
+ */
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n < 0);
+	if (n > 0);
 	{
 		printf("%n is positive\n")
 	}
-	else (n = 0);
+	else if (n = 0);
 	{
 		printf("%n is zero\n");
 	}
-	else (n > 0);
+	else (n < 0);
 	{
 		printf("%n is negative\n");
 	}
