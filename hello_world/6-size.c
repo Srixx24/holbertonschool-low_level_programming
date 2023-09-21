30ac62c5bf478f0d00c11fdfaa98499d50819cf9
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-09-20 20:01:00 -0700

    edit in all lines and a comment

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-/*File: 6-size.c
+/*
+ * File: 6-size.c
  * By: Jackie L
  */
 int main(void)
@@ -7,11 +8,11 @@ int main(void)
 	/*
 	 * Main - Printing size of types in bytes
 	 */
-	printf("Size of a char: 1 byte(s)");
-	printf("Size of an int: 4 byte(s)");
-	printf("Size of a long int: 4 byte(s)");
-	printf("Size of a long long int: 8 byte(s)");
-	printf("Size of a float: 4 byte(s)");
+	printf("Size of a char: %lu byte(s)\n", sizeof (char));
+	printf("Size of an int: %lu byte(s)\n", sizeof (int));
+	printf("Size of a long int: %lu byte(s)\n", sizeof (long int));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof (long long int));
+	printf("Size of a float: %lu byte(s)\n", sizeof (float));
 
 	return (0);
 }
