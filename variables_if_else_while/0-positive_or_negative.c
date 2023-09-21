eb048f41fad7270ecc3c75114fa0cc093e1aa7c1
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-09-21 09:22:37 -0700

    first push

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,8 +1,13 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
+#include <stdio.h>
 
-/* betty style doc for function main goes there */
+/**
+ * Main - printing variables based on n's value
+ *
+ * Return: 0
+ *
+ * */
 int main(void)
 {
 	int n;
