4973d8127029cf163493bf130e12b93bcfe48154
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-09-20 18:56:00 -0700

    more doc...

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 /**Documentation for puts function
- * Jackie L
  *
- * first attempt to put together a puts fuction
+ * By: Jackie L
+ *
  */
 int main(void)
 {
 	/**Start*/
 	puts("\"Programming is like building a multilingual puzzle");
-
+	/**My first attempt of a puts funtion*/
 	return (0);
 }
