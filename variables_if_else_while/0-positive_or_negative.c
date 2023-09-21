27b8930225ef4f5cd0cb940a5437f804a3b5e7ae
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-09-21 10:10:53 -0700

    small edit

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -5,6 +5,8 @@
 /**
  * Main - printing variables based on n's value
  *
+ * Description: printing variables based on n's value
+ *
  * Return: 0
  *
  */
@@ -15,11 +17,14 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	if (n > 0) {
+	if (n > 0) 
+	{
 		printf("is positive\n");
-	} else if (n < 0) {
+	} else if (n < 0) 
+	{
 		printf("is negative\n");
-	} else {
+	} else 
+	{
 		printf("is zero\n");
 	}
 
