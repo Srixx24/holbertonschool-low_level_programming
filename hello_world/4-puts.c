7af62dea4e1086b0dfee64e2060a601d9c4e46e7
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-09-20 18:12:14 -0700

    small edits

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <string.h>
 
-int main () {
+int main() {
 
-	char str1[] = "Programming is like building a multilingual puzzle";\n
+	char str1[] = "Programming is like building a multilingual puzzle";
+	\n;
 
 	puts(str1);
 
