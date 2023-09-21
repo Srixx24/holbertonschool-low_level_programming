9ab4ce30852531d8f27c51465eef8c936559f70d
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-09-21 08:20:11 -0700

    comment edit

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
-/*
- * File: 6-size.c
- * By: Jackie L
+/**
+ * main - puts function
+ *
+ * Description: prints lines of text with puts
+ *
+ * Return: 0
  */
 int main(void)
 {
-	/*
-	 * Main - Printing size of types in bytes
-	 */
 	printf("Size of a char: %lu byte(s)\n", sizeof(char));
 	printf("Size of an int: %lu byte(s)\n", sizeof(int));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
