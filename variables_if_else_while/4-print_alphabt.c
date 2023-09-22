bb0300b7500d59f6808306f9f1a0cab68d4d9257
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-09-21 22:41:30 -0700

    first push

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - printing alphabet with putchar function
+ *
+ *Discription - using putchar fuction to print all
+ *alphabet except q and e
+ *
+ *Return: 0
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch == 'q' && 'e' )
+	}
+	{
+		putchar (ch);
+	}
+	putchar ('\n');
+
+	return (0);
+}
