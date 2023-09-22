79b137508d3b737f15fcf2fd75a681e9781330a5
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-09-21 18:50:19 -0700

    checkin

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -20,16 +20,15 @@ int main(void)
 
 	if (x > 5)
 	{
-		printf("Last digit of %x is %x and is greater than 5\n, n, x");
+		printf("Last digit of %d is %d and is greater than 5\n", n, x);
 	
-	} else if  ( x < 6 | (n %10) != 0; )
+	} else if  ( x < 6 && ( n % 10 ) != 0; )
 	{
-		printf("Last digit of %x is %x and is less than 6 and not 0\n, n, x");
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, x);
        	} else
 	{
-		printf("Last digit of %x is %x and is 0\n, n, x")
-	}
-													     
+		printf("Last digit of %d is %d and is 0\n", n, x);
+	}		
 
 
 	return (0);
