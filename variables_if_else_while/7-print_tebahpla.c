2c66c574fdf53f4c0c72b1b97de653242e0983db
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-09-22 14:08:34 -0700

    first push

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - printing alphabet with putchar function
+ *
+ * Discription - using putchar fuction to print all
+ * lowercase alphabet.
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z'; ch <= 'a'; ch++)
+	{
+
+		putchar (ch);
+
+							        }
+		putchar ('\n');
+
+	return (0);
+}
+
