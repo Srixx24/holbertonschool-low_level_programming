00f7504991ce47aa7f294edd1a3470397701afe0
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-09-22 13:51:43 -0700

    first push

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - program that prints all single digit
+ * numbers of base 10 starting from 0
+ *
+ * discription -  program that prints all single digit
+ * numbers of base 10 starting from 0 with new line
+ * with putchar fuction
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int x;
+
+	for (x - 48; x < 58; x++);
+	{
+		putchar (x);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
