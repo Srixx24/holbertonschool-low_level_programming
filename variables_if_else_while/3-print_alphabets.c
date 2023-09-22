8470fad5e8927e937091ca0635c80096c95a4c81
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-09-21 22:17:30 -0700

    compile error corrections

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -11,14 +11,12 @@ int main(void)
 {
 	char ch;
 
-	for (ch = 'a'; ch<= 'z'; ch++);
-	{
-	putchar(ch);
+	for(ch = 'a'; ch<= 'z'; ch++) {
+		putchar(ch);
 	}
 
-	for (ch = 'A'; ch <= 'Z'; ch++);
-	{
-	putchar(ch);
+	for(ch = 'A'; ch <= 'Z'; ch++) {
+		putchar(ch);
 	}
 
 	putchar('\n');
