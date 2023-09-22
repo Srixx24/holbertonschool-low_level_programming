e965ad7fc13f1417b34f1280b9f614917c39e9e1
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-09-22 11:37:37 -0700

    first push

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - printing numbers
+ *
+ * Description - program that prints all single digit
+ * numbers of base 10 starting from 0
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		printf("%d", x);
+		if (x < 9)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n")
+	
+	return(0);
+}
