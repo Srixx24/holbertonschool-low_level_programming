be8dc32de61acc9a29a98c489f62b0883066eac2
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-09-21 20:58:26 -0700

    first push

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - putchar function used to print letters
+ *
+ * description - printing aplabet upper and
+ * lowercase with putchar function.
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char ch;
+	for
+	(ch = 'A'; ch = 'Z'; ch++)
+	(cha = 'a'; ch = 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n')
+	}
+
+	return (0)
+}
