48f53fc09e24129f06aed22ee8b8f687dc384830
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-09-21 17:55:41 -0700

    checking format

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -16,17 +16,18 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-			
-	if ((n % 10) > 5)
+	int x = n % 10;		
+
+	if (x > 5)
 	{
-		printf("Last digit of %d is %d and is greater than 5\n");
+		printf("Last digit of %d is %d and is greater than 5\n, n, x");
 	
-	} else if  ((n % 10) < 6 | (n %10) != 0; )
+	} else if  ( x < 6 | (n %10) != 0; )
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n");
-       	} else ("n" == 0)
+		printf("Last digit of %d is %d and is less than 6 and not 0\n, n, x");
+       	} else
 	{
-		printf("Last digit of %d is %d and is 0\n")
+		printf("Last digit of %d is %d and is 0\n, n, x")
 	}
 													     
 
