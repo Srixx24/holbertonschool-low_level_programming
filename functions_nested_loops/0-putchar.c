62831dc4e37ef64fdb1abd2028ce078ef9d93410
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-09-22 21:52:42 -0700

    header edit

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,9 +1,20 @@
+#include <stdio.h>
 #include "main.h"
+
+#ifndef HEAD_H
+#define HEAD_H
+_putchar 
+#endif
+
 /**
  * main - program that prints _putchar, followed by a new line
  *
  * Return: 0
- */
+*/
+
+float f_to_c(float F);
+float c_to_f(float C);
+
 int main(void)
 {
 	printf ("_putchar\n");
