fc455ae29ae97e31e8e9c8da76a65816d391dcc2
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-09-22 19:21:08 -0700

    first push

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Write a program that prints all possible
+ * combinations of single-digit numbers separated
+ * by , printed in ascending order with putchar
+ * function x4
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+
+		putchar(x + '0');
+		if (x < 9)
+
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n')
+	return (0); 
+}
