796b15a79c39fec4f0ad70d898cf1d4aff64d207
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-09-23 21:53:13 -0700

    first push

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * islower - Writing a function that checks
+ * for lowercase characters that returns 1
+ * if c is lowercase otherwise 0
+ *
+ * Return: 0 or 1
+ */
+
+int _islower(int c);
+{
+	char ch = 'x';
+
+	if (islower(ch) ! = 0
+		{
+		printf("The given character is a lowercase letter.\n");
+		}
+	else 
+		{
+		("The given character is not a lowercase letter.\n");
+		}
+	return (0);
+}
