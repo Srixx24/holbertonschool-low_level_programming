46e38351904e7dbc043c24ca585958c4c2b831dc
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-09-24 11:53:35 -0700

    first push

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#indlude "main.h"
+/**
+ * print_last_digit - function that prints
+ * the last digit of a number.
+ *
+ * Return: 0
+ */
+int print_last_digit(int x)
+{
+	int last;
+	
+	last = x % 10;
+	if (last <0)
+	{
+		last = last * -1;
+	}
+
+	return (last);
