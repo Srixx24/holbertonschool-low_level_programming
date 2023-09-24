781da411c660c956adee3edf26d8ba2561528dab
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-09-24 10:58:15 -0700

    first push

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _abs - function that computes the
+ * absolute value of an integer
+ *
+ * Return: 0
+ */
+int _abs(int)
+{
+	int x;
+
+	x = abs(-98);
+	
+	return (0);
+}
