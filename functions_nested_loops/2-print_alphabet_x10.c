bf3edfc88f2a922eabd493fdf044c28ee664ebf9
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-09-23 18:47:54 -0700

    corrections

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * print_alphabet_x10 - prototype print alphabet
  * using _putchar fuction to print all
@@ -13,7 +14,7 @@ void print_alphabet_x10(void)
 
 	for (i = '0'; i < 10; i++)
 	{
-		for (ch = 'a'; ch <= 'z'; ch++)
+		for (ch = 'a'; ch <= 'z'; ch++){
 			printf("%c", ch);
 	}
 	{
