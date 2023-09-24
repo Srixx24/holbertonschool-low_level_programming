8c232589c9bc7ec7c3031f67090b2e5ab71ecc96
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-09-24 13:06:05 -0700

    compiler fix

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -3,7 +3,7 @@
 /**
  * jack_bauer - function that prints every minute
  * of the day of Jack Bauer, starting from
- * 00:00 to 23:59 
+ * 00:00 to 23:59
  *
  * Return: 0
  */
@@ -22,6 +22,6 @@ void jack_bauer(void)
 			_putchar(m % 10 + '0');
 			_putchar('\n');
 		}
-	
+
 	}
 }
