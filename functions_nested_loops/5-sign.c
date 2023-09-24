4dd6e0d5d52d55c13029bb11b3d23fdfac5b6f03
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-09-24 10:16:13 -0700

    compiler fix

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -7,7 +7,7 @@
  * n - if greater than 0 prints + and returns 1,
  * if is 0 prints 0 and returns 0, if less than 
  * 0 prints - and returns -1
- *
+ * 
  * Return: 1, 0, or -1
  */
 int print_sign(int n)
@@ -17,7 +17,7 @@ int print_sign(int n)
 	putchar (43);
 	return (1);
 	}
-	if else (n< 0)
+	if else ( n< 0)
 	{
 	putchar (45);
 	return (-1);
