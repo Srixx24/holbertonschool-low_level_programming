8b8d1584bcc9bbf686483514ead8be45f9f00437
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-09-24 09:52:48 -0700

    first push

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _isalpha - function that checks for alphabetic
+ * character returns 1 if c is a letter, lower or upper case
+ *
+ * Return: 1 if C is a letter otherwise 0
+ */
+
+int _isalpha(int c);
+{
+	if (c >= 65 && c <= 122)
+	{
+		return (1);
+	}
+	return (0);
+} 
