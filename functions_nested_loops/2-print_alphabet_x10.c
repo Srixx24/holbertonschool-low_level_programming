84cba4de18a0245c17f721facef972a5f0512a61
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-09-23 18:40:41 -0700

    output correction

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,8 @@
 #include "main.h"
 /**
- * void print_alphabet_x10(void) - printing alphabet with p_utchar function
- *
+ * print_alphabet_x10 - prototype print alphabet
+ * using _putchar fuction to print all
+ * lowercase alphabet with prototype ten times
  * Return: 0
  */
 
@@ -10,8 +11,8 @@ void print_alphabet_x10(void)
 	char ch;
 	int i;
 
-	for (i = '0'; i < 10; i++)
-		for (ch = 'a'; ch <= 'z'; ch++)
+	for (i = '0'; i < 10; i++){
+		for (ch = 'a'; ch <= 'z'; ch++)}
 	{
 
 		_putchar (ch);
