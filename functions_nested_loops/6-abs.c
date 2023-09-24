a7682cd03ee8531c9641f301d66a825a61f018a7
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-09-24 11:39:54 -0700

    compiler edit

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -4,21 +4,21 @@
 /**
  * _abs - function that computes the
  * absolute value of an integer
- * 
- * x - absolute value of an integer 
+ *
+ * x - absolute value of an integer
  *
  * Return: 0
  */
 int _abs(int x)
 {
 	if (x < 0)
-	
+
 	{
 		int abs_val;
 
 		abs_val = x * -1;
-		return(abs_val);
+		return (abs_val);
 	}
 
-	return (0);
+	return (x);
 }
