0a1df267a26ca6f6c86fe0795d06b91fb0b3d9c4
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-09-24 12:14:33 -0700

    compiler edit

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -4,18 +4,20 @@
  * print_last_digit - function that prints
  * the last digit of a number.
  *
+ * @x: number for parameter
+ *
  * Return: 0
  */
 int print_last_digit(int x)
 {
 	int last;
-	
+
 	last = x % 10;
-	if (last <0)
+	if (last < 0)
 	{
 		last = last * -1;
 	}
-	putchar(last + 0);
+	putchar(last + '0');
 
 	return (last);
 }
