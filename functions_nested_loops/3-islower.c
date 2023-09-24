cdbc39c82f045b1634ffd169797daf39aabb72d5
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-09-23 22:23:24 -0700

    compiler fix

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -4,8 +4,8 @@
  * _islower - Writing a function that checks
  * for lowercase characters that returns 1
  * if c is lowercase otherwise 0
- * 
- * C - if lower return 1 otherwise 0
+ *
+ * #C - if lower return 1 otherwise 0
  *
  * Return: 0 or 1
  */
