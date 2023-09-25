ce1002da0f998d5ffbcc4fcc0550af2d8d8a9e0b
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-09-25 06:27:05 -0700

    first push

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * add - function that adds two integers
+ * and returns the results
+ *
+ * Return: x or y
+ */
+int add(int x, int y)
+{ 
+	return (x + y);
+}
---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,11 @@
+#include <stdio>
+#include "main.h"
+/**
+ * times_table - function that prints the
+ * 9 times table, starting with 0
+ *
+ * Return: 0
+ */
+void times_table(void)
+{
+	int 
