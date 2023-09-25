75d103df455a12c0ff81dc22890d9966d99c9212
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-09-25 07:38:01 -0700

    first push

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_to_98 - function that prints all
+ * natural numbers from n to 98 followed by
+ * a , and in order
+ *
+ * Return: 0
+ */
+void print_to_98(int n)
+{
+	if (n <= 98);
+	{
+		printf("%2d", n);
+	}
+	return (0);
+}
