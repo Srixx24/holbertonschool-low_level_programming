d4c5365aae6c5d2098d45bca46bc58c27fa4f9de
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-09-25 08:23:00 -0700

    compiler edits

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -11,15 +11,21 @@
  */
 void print_to_98(int n)
 {
-	int n;
 
-	if (n >= 98)
+	if (n > 98)
 	{
-		while (n > 98)
-		printf("%d,", n);
+		while (n >= 98)
+		{
+			printf("%d,", n);
+			if (n != 98)
+			{
+				print(", ")
+			}
+			n--;
+		}
 	}
-	else (n = 98);
+	else
 	{
-		printf("%d");
+		printf("98");
 	}
 }
