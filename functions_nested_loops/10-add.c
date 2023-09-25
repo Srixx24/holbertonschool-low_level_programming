d77dd0015665cabb9b7aca65d44faee2f3007b9a
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-09-25 06:29:38 -0700

    first push

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -4,9 +4,12 @@
  * add - function that adds two integers
  * and returns the results
  *
+ * @x: First value
+ * @y: Second value
+ *
  * Return: x or y
  */
 int add(int x, int y)
-{ 
+{
 	return (x + y);
 }
