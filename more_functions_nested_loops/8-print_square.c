2ec47e6156e8a460f8114dd9b8576254bb9881eb
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-09-27 16:37:28 -0700

    first push

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_square - function that prints a square,
+ * followed by a new line. Where 'size' is the
+ * size of the square. If size is 0 or less, the
+ * function should print only a new line. The 
+ * character # to print the square.
+ *
+ * @size:
+ *
+ * Return: 0
+ */
+void print_square(int size)
+{
+	int x;
+	
+	for (x = 0; x > 10; x++)
+	{
+		putchar('##########');
+		putchar('\n');
+	}
+	putchar('\n');
+}
