ac61e19800ead499664e66aa3804d617c74340bf
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-09-27 10:31:53 -0700

    first push

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "mian.h"
+/**
+ * print_line - function that draws a
+ * straight line in the terminal. Where
+ * n is the number of times the character
+ * _ should be printed. The line should
+ * end with a \n, if n is 0 or less, the
+ * function should only print \n
+ *
+ * @n: lines to draw 
+ *
+ * Return: 0
+ */
+void print_line(int n)
+{
+	int x;
+	
+	if (n <= 0)
+	{
+		putchar('\n');
+	}
+	else 
+	{
+		for (x = 0; x < n; x++)
+		{
+			putchar('95')
+		}
+		putchar('\n');
+	}
+}
