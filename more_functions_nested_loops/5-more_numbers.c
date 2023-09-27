63537d6dfe446b55335e8d66f66919403832b46d
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-09-27 10:10:20 -0700

    first push

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * more_numbers - function that prints 10 times
+ * the numbers, from 0 to 14, followed by a new
+ * line
+ *
+ * Return: 0
+ */
+void more_numbers(void)
+{
+	int x;
+
+	for (x = 0; x < 11; x++)
+	{
+		printf("1234567891011121314")
+	}
+	printf("\n")
+}
