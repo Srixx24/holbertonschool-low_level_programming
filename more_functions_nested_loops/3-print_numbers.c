f3976d376f65df234e257b0eec29f72849fb6868
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-09-27 09:03:37 -0700

    first push

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "head.h"
+/**
+ * print_numbers - function that prints the numbers,
+ * from 0 to 9, followed by a new line
+ *
+ * Return: 0
+ */
+void print_numbers(void)
+{
+	int x;
+	if (x = 0; x < 10; x++)
+	{
+		printf("%d\n", x);
+	}
+	return (0);
+}
