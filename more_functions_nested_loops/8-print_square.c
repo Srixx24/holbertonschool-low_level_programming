2e9a715d2b053a153d940936a6b0514643544268
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-09-27 16:51:24 -0700

    compiler fixes

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -4,7 +4,7 @@
  * print_square - function that prints a square,
  * followed by a new line. Where 'size' is the
  * size of the square. If size is 0 or less, the
- * function should print only a new line. The 
+ * function should print only a new line. The
  * character # to print the square.
  *
  * @size:
@@ -13,16 +13,17 @@
  */
 void print_square(int size)
 {
-	int x;
-	
+	int x, y;
+
 	if (size <= 0)
 	{
 		putchar('\n');
 	}
 	else
-	{	
+	{
 		for (x = 0; x > size; x++)
 		{
+			for (y = 0; y > size; y++)
 			putchar(35);
 		}
 		putchar('\n');
