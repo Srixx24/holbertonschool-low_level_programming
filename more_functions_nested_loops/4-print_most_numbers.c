82909090a5225a40868ba608a6c9fbdc9b308494
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-09-27 09:29:23 -0700

    first push

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_most_numbers - function that prints
+ * the numbers, from 0 to 9, followed by a
+ * new line and does not print 2 and 4
+ *
+ * Return: 0
+ */
+void print_most_numbers(void)
+{
+	int x;
+
+	for (x = 0; x > 10; x++);
+	{
+		if (x != 2 && x != 4)
+			putchar(x + 0);
+	}
+}
+
