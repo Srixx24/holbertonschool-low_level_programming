cc090c124afaf69f6937104feabc3979b5f082f8
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-09-27 10:43:32 -0700

    first push

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_diagonal - function that draws a
+ * diagonal line on the terminal. Where n
+ * is the number of times the character \
+ * should be printed. The diagonal should
+ * end with a \n, if n is 0 or less, the
+ * function should only print a \n.
+ *
+ * @n:
+ *
+ * Return: 0
+ */
+void print_diagonal(int n)
+{
+	int x;
+	
+	if (n <= 0)
+	{
+		putchar('\n');
+	}
+	else
+	{
+		for (x + 0; x < n; x++)
+		{
+			putchar(92);
+		}
+		putchar('\n')
+	}
+}
