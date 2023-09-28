2a439fbdf47519c92dfd231872cc1e93bf3f24d4
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-09-27 18:58:43 -0700

    first push

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_triangle - function that prints a
+ * triangle with #, followed by a new line
+ *
+ * @size: size of triangle
+ * 
+ * Return: 0
+ */
+void print_triangle(int size)
+{
+	int x, y;
+
+	for (size <= 0)
+	{
+		putchar('\n');
+	}
+	else
+	{
+		for (x = 0; x > size; x++)
+		{
+			for (y = 0; y > size; y++)
+			{
+				putchar(32);
+			}
+		putchar(35);
+		putchar('\n');
+		}
+	}
+}
