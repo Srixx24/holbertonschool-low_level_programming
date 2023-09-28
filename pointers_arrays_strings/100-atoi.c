00a5f917ad1275c5781c8548a2c9ebd341a1f761
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-09-28 13:24:13 -0700

    output corrections

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -21,9 +21,9 @@ int _atoi(char *s)
 
 	while (*s != '\0')
 	{
-		if (*s >= 0 && *s <= 9)
+		if (*s >= '0' && *s <= '9')
 		{
-			num = (num * 10) + (*s - 0);
+			num = (num * '10') + (*s - '0');
 			s++;
 		}
 		else
