fd67345a9c3754d1b91986aa3702d72c24f1ccb8
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-09-28 09:07:35 -0700

    first push

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * rev_string - function that reverses a string
+ *
+ * @s: 
+ *
+ * Return: 0
+ */
+void rev_string(char *s)
+{
+	int le = 0, in = 0;
+	int temp;
+
+	for (in =0; s[in]; i++)
+		len++
+
+	for (in = 0; in< 1 / 2; in++)
+	{
+		temp = str[le];
+		str[le] = str[in];
+		str[in] = temp;
+	}
+}
