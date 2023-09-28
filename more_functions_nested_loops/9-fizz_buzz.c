b07334303f5e417d01b5aee7f5693ee9878d9404
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-09-27 18:31:53 -0700

    compiler fixes

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -14,6 +14,10 @@ int main(void)
 
 	for (x = 1; x <= 100; x++)
 	{
+		if ((x % 3 == 0) && (x % 5 == 0))
+		{
+			printf("FizzBuzz ");
+		}
 		if ((x % 3) == 0)
 		{
 			printf("Fizz ");
@@ -21,10 +25,6 @@ int main(void)
 		else if ((x % 5) == 0)
 		{
 			printf("Buzz ");
-		}
-		else if ((x % 3 == 0) && (x % 5 == 0))
-		{
-			printf("FizzBuzz ");
 		}
 		else
 		{	
