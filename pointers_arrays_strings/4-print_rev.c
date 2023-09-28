6fb94a43ca9333d056963dce65d1cde30a75bd22
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-09-28 08:29:56 -0700

    compiler fixes

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
+#include <string.h>
 #include "main.h"
 /**
  * print_rev - function that prints a string,
  * in reverse, followed by a new line
  *
- * @s:
+ * @s: string length
  *
  * Return: 0
  */
 void print_rev(char *s)
 {
-	int length = strlen(s);
+	int length = s;
 	int x, y;
 
 	for (x = 0, y =length -1; x <y; x++, y--)
