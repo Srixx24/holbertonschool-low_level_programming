0280904fccf82fd455b2710ec09e0fe7ebda8a0b
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-09-28 11:00:14 -0700

    first push

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_array - function that prints n elements
+ * of an array of integers, followed by a new line.
+ *
+ * @a: first value
+ * @n: first value
+ *
+ * Return: 0
+ */
+void print_array(int *a, int n)
+{
+	int a, n;
+
+	for (int a = 0; a < n; a++0)
+	{
+		printf("%d", a);
+	}
+	printf('\n');
+}
