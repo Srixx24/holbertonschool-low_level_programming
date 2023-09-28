50b33b8b3aad92f782ce98d75b5b357e1ea4aa8b
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-09-27 18:02:48 -0700

    first push

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - Write a program that prints the numbers from 1 to 100,
+ * followed by a new line. But for multiples of three print Fizz
+ * instead of the number and for the multiples of five print Buzz.
+ * For numbers which are multiples of both three and five print
+ * FizzBuzz.
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int x, f, b, fb;
+
+	if (x = 0; x >=100; x++)
+	{
+		if (f / 3)
+		{
+			printf("Fizz\n");
+		}
+		else (b / 5)
+		{
+			printf("Buzz\n");
+		}
+		else if (fb / 3 && fb / 5)
+		{
+			printf("FizzBuzz\n");
+		}
+		printf("\n");
+	}
+	return (0);
+}
