e0150bc0cdeb05430f976af5d54abe70d498f0dd
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-09-28 07:08:23 -0700

    first push

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * swap_int - function that swaps the
+ * values of two integers
+ *
+ * @a: first value
+ * @b: second value
+ *
+ * Return: 0
+ */
+void swap_int(int *a, int *b)
+{
+	int a, b;
+
+	swap(&a, &b);
+}
