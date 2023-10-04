68f8f40339466d17af78096a4179aa4cc4de6569
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-10-04 15:21:09 -0700

    Output Edits

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -18,7 +18,7 @@ char *_strncat(char *dest, char *src, int n)
 
 	for (i = 0; i <= n && src[i] != '\0'; i++)
 	{
-		dest[dest_len + i] = src;
+		dest[dest_len + i] = src[i];
 	}
 	dest[dest_len + i] = '\0';
 
