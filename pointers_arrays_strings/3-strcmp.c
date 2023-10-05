96acfd52453a72c0d4ca9f3b7c30b68c35c5a816
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-10-04 18:20:13 -0700

    First push

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+/**
+ * _strcmp - function that compares two strings.
+ *
+ * @s1: string 1
+ * @s2: string 2
+ *
+ * Return: 0
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int res = strcmp(s1, s2);
+
+	while (s1 && s2 != 0)
+	{
+		s1++;
+		s2++;
+	}
+
+	return (res)
+}
