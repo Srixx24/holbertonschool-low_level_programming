1277a6b94e8687b750baa3be3ddd5654c4f406be
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-10-04 19:39:01 -0700

    Output fix

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -19,7 +19,7 @@ int _strcmp(char *s1, char *s2)
 	}
 	if (*s1 == '\0' && *s2 == '\0')
 	{
-		return (0); 
+		return (0);
 	}
 	else if (*s1 == '\0')
 	{
@@ -29,6 +29,6 @@ int _strcmp(char *s1, char *s2)
 	{
 		return (-1);
 	}
-	
+
 	return (*s1 - *s2);
 }
