9bad38011f319d265a906d9fdc2dea79bbf9c983
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-10-04 20:10:52 -0700

    first push

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+/**
+ * string_toupper - function that changes all
+ * lowercase letters of a string to uppercase
+ *
+ * Return: 0
+ */
+char *string_toupper(char *)
+{
+	int i;
+
+	while (*[i])
+	{
+		*[i] = toupper;
+		i++;
+	}
+
+	return (0);
+}
