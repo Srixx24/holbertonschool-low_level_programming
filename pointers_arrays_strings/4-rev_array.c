66b6a4794079937863dc161faac7739ea74e4327
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-10-04 20:02:20 -0700

    Compiler fixes

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -18,9 +18,9 @@ void reverse_array(int *a, int n)
 
 	while (start < end)
 	{
-		temp = int *a[start];
-		int *a[start] = int *a[end];
-		int *a[end] = temp;
+		temp = int a[start];
+		int a[start] = int a[end];
+		int a[end] = temp;
 		
 		start++;
 		end--;
