840c991672f2f6ec79b111f6bbfbf5a4b874d15f
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-10-04 20:50:07 -0700

    first push

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+/**
+ * cap_string - function that capitalizes 
+ * all words of a string.
+ *
+ * @str: string
+ *
+ * Return: 0
+ */
+char *cap_string(char *str)
+{
+	char *ptr = str;
+	int cap = 1;
+
+	while (*ptr)
+	{
+		if (isspace(*ptr))
+		{
+			cap = 1;
+		}
+		else
+		{
+			*ptr = toupper(*ptr);
+			cap = 0;
+		}
+		ptr++;
+	}
+
+	return (str);
+}
