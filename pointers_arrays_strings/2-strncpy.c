c63d82faf59a3c77d7b21edb4194dbd6d7613bc5
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-10-04 17:59:22 -0700

    Compiler fixes

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -15,12 +15,14 @@ char *_strncpy(char *dest, char *src, int n)
 
 	while (*src && n > 0)
 	{
+		*dest++ = *src++;
 		n++;
 	}
 
 	while  (n > 0)
 	{
 		*dest++ = '\0';
+		n++;
 	}
 	return (dest);
 }
