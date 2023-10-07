b1a6be148e7176990d4e86e82014f407e174422e
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-10-06 18:51:18 -0700

    first push

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * leet - function that encodes a string into 1337
+ * With only use one if loop and only two loops
+ * Letters a and A should be replaced by 4
+ * Letters e and E should be replaced by 3
+ * Letters o and O should be replaced by 0
+ * Letters t and T should be replaced by 7
+ * Letters l and L should be replaced by 1
+ *
+ * @str: string
+ *
+ * Return: str
+ */
+char *leet(char *str)
+{
+	char *ptr = str;
+	char x = *ptr;
+
+	while (*ptr)
+	{
+		if (x == 'a' || x == 'A' || x == 'e' || x == 'E' || x== 'o' || x == 'O'
+		|| x == 't' || x == 'T' || x == 'l' || x == 'L')
+		{
+			*ptr = (x == 'a' || x == 'A') ? '4' :
+			(x == 'e' || x == 'E') ? '3' :
+			(x == 'o' || x == 'O') ? '0' :
+			(x == 't' || x == 'T') ? '7' :
+			(x == 'l' || x == 'L') ? '1' :
+	
+		}
+	
+	ptr++;
+
+	return (str);
+}
