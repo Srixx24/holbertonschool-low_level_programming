97754454a9ef14587a370a295f7c7533e12d091d
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-10-06 19:35:31 -0700

    first push

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _memset - function that fills memory with a constant byte.
+ *
+ * @s: pointer
+ * @b: bytes
+ * @n: number
+ *
+ * Return: 0
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	char *ptr = s;
+
+	while (n > 0)
+	{
+		*ptr = b;
+		ptr++;
+	}
+	
+	return (s);
+}	
