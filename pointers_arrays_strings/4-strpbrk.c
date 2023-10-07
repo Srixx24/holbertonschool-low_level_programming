f9f74516af3c24de7a0dfb54e3103d8552bdbfe6
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-10-07 14:18:09 -0700

    first push

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strpbrk - function that searches a string for any of a set of bytes.
+ *
+ * @s: pointer
+ * @accept: string
+ *
+ * Return: 0
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int x;
+
+	while (*s)
+	{
+		for (x = 0; accept[x]; x++)
+		{
+			if (*s == accept[x])
+			{
+				return s;
+			}
+		}
+	}
+	return (0);
+}
