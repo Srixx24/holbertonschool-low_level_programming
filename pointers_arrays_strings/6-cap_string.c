62089ee5b27df979501036d697eb42b4a2b1fe1e
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-10-06 18:13:03 -0700

    Compiler edits

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -17,7 +17,9 @@ char *cap_string(char *str)
 
 	while (*ptr)
 	{
-		if (*ptr == ' ' || *ptr == '\t' || *ptr == '\n' || *ptr == ',' || *ptr == ';' || *ptr == '.' || *ptr == '!' || *ptr == '?' || *ptr == '"' || *ptr == '(' || *ptr == ')' || *ptr == '{' || *ptr == '}')
+		if (*ptr == ' ' || *ptr == '\t' || *ptr == '\n' || *ptr == ',' || *ptr == ';' || 
+		*ptr == '.' || *ptr == '!' || *ptr == '?' || *ptr == '"' || *ptr == '(' ||
+		*ptr == ')' || *ptr == '{' || *ptr == '}')
 		{
 			cap = 1;
 		}
