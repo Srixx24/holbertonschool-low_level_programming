07ace58e479af5e9076f54edd2f6186e45813033
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-10-06 20:30:12 -0700

    first push

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _memcpy - function that copies memory area.
+ *
+ * @dest: destination string
+ * @src: source string
+ * @n: number
+ *
+ * Return: dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	char *destp = dest;
+	char *srcp = scr;
+
+	while (n > 0)
+	{
+		*destp = *srcp;
+		destp++;
+		src++; 
+		n--;
+	}
+
+	return (dest);
+}
