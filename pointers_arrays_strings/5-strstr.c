34ccb88a0a1429da7a4bedc2758565c1a6d36714
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-10-07 17:44:23 -0700

    first push

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,14 @@
+nclude <stdio.h>
+#include "main.h"
+/**
+ * _strstr - function that locates a substring.
+ *
+ * @haystack: string
+ * @needle: substring
+ *
+ * Return: needle
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	return (needle(haystack, needle));
+}
