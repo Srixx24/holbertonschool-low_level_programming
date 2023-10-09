e5a0f3f48e6f1f00601b953faeb855e2850724a4
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-10-09 06:41:02 -0700

    Compiler edit

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * void print_diagsums - function that prints the
+ * print_diagsums - function that prints the
  * sum of the two diagonals of a square matrix of
  * integers
  *
- * @a: 
- * @size: 
+ * @a: array
+ * @size: size
  *
  * Return: 0
  */
@@ -18,7 +18,7 @@ void print_diagsums(int *a, int size)
 
 	for (x = 0; x < size; x++)
 	{
-		sum1 += a[size + 1];
-		sum2 += a[size + (size - 1 -x)];
+		sum1 += a[x * size + 1];
+		sum2 += a[x * size + (size - 1 -x)];
 	}
 }
