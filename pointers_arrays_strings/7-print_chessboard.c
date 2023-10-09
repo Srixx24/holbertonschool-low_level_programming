359a933392d6e3c93e974337572b2a7403b5ccfd
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-10-09 06:22:53 -0700

    Compiler edit

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -3,14 +3,14 @@
 /**
  * print_chessboard - function that prints the chessboard.
  *
- * @a:
+ * @a: array
  *
  * Return: chessboard
  */
 void print_chessboard(char (*a)[8])
 {
 	int x, y;
-	
+
 	for (x = 0; x < 8; x++)
 	{
 		for (y = 0; y < 8; y++)
