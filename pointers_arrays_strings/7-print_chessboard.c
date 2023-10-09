69e34780d417f376006667f25a60c7cc56eef79e
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-10-09 06:12:24 -0700

    first push

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_chessboard - function that prints the chessboard.
+ *
+ * @a:
+ *
+ * Return: chessboard
+ */
+void print_chessboard(char (*a)[8])
+{
+	int x:
+	int y;
+	
+	for (x = 0; x < 8; x++)
+	{
+		for (y = 0; < 8; y++)
+	}
+}
