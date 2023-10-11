4db808c60c930e0e061cf7749769518b0c799ba1
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-10-11 15:30:18 -0700

    First Push

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _print_rev_recursion - prints a string in reverse.
+ *
+ * @s: string
+ *
+ * Return: 0
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		putchar('\n');
+	}
+	else
+	{
+		putchar (*s);
+		_print_rev_recursion(s +1);
+	}
+}
