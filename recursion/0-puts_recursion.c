2c50603eb54e7334da6b95dbfb2f7c718f7624cd
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-10-11 12:12:24 -0700

    first push

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * void _puts_recursion(char *s) - function that prints a string, followed by a new line.
+ *
+ * @s: string
+ *
+ * Return: 0
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		putchar '\n';
+	}
+	else 
+	{
+		putchar("%c", *s);
+	}
+}
