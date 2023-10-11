48c131b00baadc0c804550b1e48f10ef201d14d3
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-10-11 15:42:13 -0700

    First push

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strlen_recursion - returns the length of a string
+ *
+ * @s: String
+ *
+ * Return: string length
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	else 
+	{
+		_strlen_recursion(s +1);
+	}
+}
