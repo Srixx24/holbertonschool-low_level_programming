11f27eb959f1237cddef40ed89068bac2ed3cae9
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-10-11 16:40:52 -0700

    First push

---
+++ recursion/3-factorial.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * factorial - returns the factorial of a given number
+ *
+ * @n:
+ *
+ * Return: 0, 1, -1
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (n);
+	}
+}
