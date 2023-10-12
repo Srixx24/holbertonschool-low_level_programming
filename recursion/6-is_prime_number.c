a76d2ca5f1afc6719c6e378bf98b2bfe36c998ef
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-10-11 21:02:13 -0700

    Compiler fix

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * is_prime_number - returns 1 if the input integer
+ * is a prime number, otherwise return 0
+ *
+ * @n: number
+ *
+ * Return: 1 or 0
+ */
+int is_prime_number(int n)
+{
+	int x = 2;
+
+	if (n <= 1)
+	{
+		return (0);
+	}
+	
+	for (x * x <= x; x++)
+	{
+		return (0);
+	}
+}
