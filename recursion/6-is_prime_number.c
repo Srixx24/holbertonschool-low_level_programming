5c2d7c6b499591b9a4ba2979ea450ca590be0041
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-10-12 17:12:30 -0700

    Compiler edit

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -9,22 +9,25 @@
  *
  * Return: 1 or 0
  */
-int is_prime_number(int n, int x)
+int is_prime_recursive(int n, int x)
 {
-	if (n < 2)
+	if (n < 2) 
 	{
-		return (0);
+	return (0);
 	}
-
 	if (x == 1)
 	{
-		return (1);
+	return (1);
 	}
-
 	if (n % x == 0)
 	{
-		return (0);
+	return (0);
 	}
 
-	return (is_prime_number (n, x - 1));
+	return (is_prime_recursive(n, x - 1));
+}
+
+int is_prime_number(int n)
+{
+	return (is_prime_recursive(n, n - 1));
 }
