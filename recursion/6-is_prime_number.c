2aabddf412ea8b9fc24dc196084c397070251c6e
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-10-12 17:20:20 -0700

    Compiler edit

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -4,7 +4,7 @@
  * is_prime_number - returns 1 if the input integer
  * is a prime number, otherwise return 0
  *
- * is_prime_recursive - recursive call
+ * is_prime_recursive - checks if a number is prime
  *
  * @n: number
  * @x: divisor
