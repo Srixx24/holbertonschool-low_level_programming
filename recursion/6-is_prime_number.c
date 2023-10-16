cf550f58c2fe99394c24a4098574cfed0ae08910
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-10-16 08:28:26 -0700

    Compiler edit

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,9 +1,6 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * is_prime_number - returns 1 if the input integer
- * is a prime number, otherwise return 0
- *
  * is_prime_recursive - checks if a number is prime
  *
  * @n: number
@@ -28,6 +25,14 @@ int is_prime_recursive(int n, int x)
 
 	return (is_prime_recursive(n, x - 1));
 }
+/**
+ * is_prime_number - returns 1 if the input integer
+ * is a prime number, otherwise return 0
+ * 
+ * @n: number
+ *
+ * return 1 or 0
+ */
 
 int is_prime_number(int n)
 {
