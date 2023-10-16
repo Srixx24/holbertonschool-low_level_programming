ae420a1a9bf6e7db5e64154ea45beea83875858d
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-10-16 07:00:02 -0700

    Compiler edit

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -18,14 +18,12 @@ int main(int argc, char *argv[])
 		x1 = atoi(argv[1]);
 		x2 = atoi(argv[2]);
 		printf("%d\n", x1 * x2);
-		
-		else 
-		{
-			printf ("Error\n");
-			return (1);
-		}
 	}
-
-
+	else 
+	{
+		printf ("Error\n");
+		return (1);
+	}
+	
 	return (0);
 }
