940f40244027ddcab5c958801cee83d2f33e4925
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-10-16 07:18:54 -0700

    Compiler edit

---
+++ argc_argv/4-add.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - program that adds positive numbers
+ *
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int x, y;
+
+	if (argc <=1)
+	{
+		printf("0\n");
+		return(0);
+	}
+	else if (x = 0; x < argc; x++)
+	{
+		char *arg = argv[x];
+		for (y = 0; arg[y] != '\0'; y++)
+		{
+			if (isdigit(arg[y]))
+			{
+				printf("Error\n");
+				return(1);
+			}
+		}
+	}
+
+	return (0);
+}
