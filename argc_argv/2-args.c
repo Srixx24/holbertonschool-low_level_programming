27f7b40b99f5b60cf22db243bc3da76ea3c1c8b9
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-10-16 06:36:06 -0700

    Compiler edit

---
+++ argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - program that prints all arguments it receives.
+ *
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int x;
+
+	for (x = 0; x < argc; x++)
+	{
+	printf("%s\n", argv[x]);
+	}
+
+	return (0);
+}
