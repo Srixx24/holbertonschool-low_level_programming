1ab475a0ecfb92612d9ba68af0e868bbe8dd9f5d
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-10-16 06:20:21 -0700

    Compiler edit

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
+/**
+ * main - program that prints its name, followed by a new line
+ */
 
-int main(void)
+int main(int argc, char *argv[])
 {
-	printf("Program name\n");
+	(void) argc;
+	printf("%s\n", argv[0]);
+
 	return (0);
 }
