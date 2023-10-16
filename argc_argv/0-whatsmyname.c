b815c9c7403aa233c7a50b67c1b55587dd8203cc
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-10-16 06:22:04 -0700

    betty

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 /**
- * main - program that prints its name, followed by a new line
+ * main - program that prints its name,
+ * followed by a new line
+ *
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0
  */
 
 int main(int argc, char *argv[])
