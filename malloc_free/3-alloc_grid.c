732a6d247474aec4be233642758f85a7746c993b
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-10-18 21:10:11 -0700

    compiler edit

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -34,7 +34,7 @@ int **alloc_grid(int width, int height)
 	}
 	for (x = 0; x < height; x++)
 	{
-		a[x] = malloc(sizeof(int *) * width);
+		a[x] = malloc(sizeof(int) * width);
 
 		if (a[x] == NULL)
 		{
