f0845d5f3a835265f603ebf0d911e5d00f605dd5
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-10-18 17:28:38 -0700

    compiler edit

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -18,13 +18,13 @@ char *create_array(unsigned int size, char c)
 	
 	if (size == 0)
 	{
-		return ('\0');
+		return (NULL);
 	}
 	a = malloc(size * sizeof(char));
 
-	if (a == '\0')
+	if (a == NULL)
 	{
-		return ('\0');
+		return (NULL);
 	}
 	for (x = 0; x < size; x++)
 	{
