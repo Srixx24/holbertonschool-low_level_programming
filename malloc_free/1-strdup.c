c65333801c548376d7eeed905298a33be48eb70a
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-10-18 18:48:49 -0700

    first push

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _strdup - function that returns a pointer to a newly
+ * allocated space in memory, which contains a copy of
+ * the string given as a parameter
+ *
+ * @str: string pointer
+ *
+ * Return: null
+ */
+char *_strdup(char *str)
+{
+	int x = 0;
+	int y = 1;
+	char *ds;
+	
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	
+	ds = malloc((sizeof(char) * y) + 1);
+
+	if (ds == NUll)
+	{
+		return (NULL);
+	}
+
+	ds[x] = '\0';
+	return (ds);
+}
