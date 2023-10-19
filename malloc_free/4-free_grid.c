8735fcaec6fdc701d695c51e39abef0b5a5c010c
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-10-18 21:19:56 -0700

    compiler edit

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * free_grid - function that frees a 2 dimensional grid
+ * previously created by your alloc_grid function
+ *
+ * @grid: grid of arrays
+ * @height: height
+ *
+ * Return: 
+ */
+void free_grid(int **grid, int height)
+{
+	int x;
+
+	if (grid == NULL)
+	{
+		return (NULL);
+	}
+
+	for (x = 0; x < height; x++)
+	{
+		free(x);
+	}
+	
+	free(grid);
+}
