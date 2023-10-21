07db00dbfa0f1c4466f784e6395efb5487eda547
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-10-20 20:52:36 -0700

    Compiler edits

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * array_range - function that creates an array of integers
+ *
+ * @min: min value
+ * @max: max value
+ *
+ * Return: NULL or range
+ */
+int *array_range(int min, int max)
+{
+	int *ar;
+	int x;
+	int size;
+
+	if (min > max)
+	{
+		return (NULL);
+	}
+	
+	size = (min - max) + 1;
+
+	ar = malloc(sizeof(int) min * max);
+
+	if (ar == NULL)
+	{
+		return (NULL);
+	}
+	for (x =0; x < size; x++)
+	{
+		ar[x] = min;
+	}
+	return (ar);
+}
