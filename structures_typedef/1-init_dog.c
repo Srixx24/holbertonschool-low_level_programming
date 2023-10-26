451e8a5914120430ed5aa332f40d574a04525415
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-10-25 17:10:56 -0700

    first push

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * init_dog - function that initialize a
+ * variable of type struct dog
+ *
+ * @d: string
+ * @name: Dogs name
+ * @age: Dogs age
+ * @owner: Owner of dog
+ *
+ * Return: 0
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	struct dog = {"Poppy, 3.5, Bob"};
+
+	return(o);
+}
