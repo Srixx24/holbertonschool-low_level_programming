665e08dbd79834d60a365c239ad5f8017988b20a
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-10-25 18:18:41 -0700

    Compiler edit

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -12,9 +12,9 @@ void print_dog(struct dog *d)
 {
 	if (d != NULL)
 	{
-		printf("name: %s\n", (d->name) ? d->name : "(nil)";
-		printf("age: %f\n", (d->age) ? d->age : "(nil)";
-		printf("owner: %s\n", (d->owner) ? d->owner : "(nil)";
+		printf("name: %s\n", (d->name) ? d->name : "(nil)");
+		printf("age: %f\n", (d->age) ? d->age : "(nil)");
+		printf("owner: %s\n", (d->owner) ? d->owner : "(nil)");
 	}
 
 	return (0);
