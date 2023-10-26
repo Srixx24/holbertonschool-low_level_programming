b98f44a14758d72d3235f0976a206f790c102700
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-10-25 17:40:55 -0700

    Compiler edit

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -14,7 +14,12 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	struct dog = name, age, owner;
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 
 	return(0);
 }
