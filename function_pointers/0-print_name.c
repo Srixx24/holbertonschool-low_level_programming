fb4f50b2658fc65e9dd1345c1a23c33b7f4e0536
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-10-27 22:47:21 -0700

    First push

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - function that prints a name
+ *
+ * @name: 
+ * @f: 
+ *
+ * Return: 0
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(%s);
+}
