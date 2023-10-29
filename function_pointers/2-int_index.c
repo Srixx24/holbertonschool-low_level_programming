24d68dcd7d97a24bfba539ba04525f9bc55b3128
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-10-28 19:12:18 -0700

    Complier edit

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * int_index - function that searches for an integer
+ *
+ * @array:
+ * @size: 
+ * @cmp:
+ *
+ * Return: 0
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int x;
+
+	if (size > 0)
+	{
+		if (arry != NULL && cmp != NULL)
+		{
+			while (x < size)
+			{
+				x++;
+			}
+		}
+	}
+}
