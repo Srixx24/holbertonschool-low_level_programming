4d3dfc0e9b74c5fefa4700370e3b94b1dae4c22c
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-10-28 20:13:23 -0700

    Complier edit

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -15,12 +15,12 @@ int sum_them_all(const unsigned int n, ...)
 	int sum = 0;
 	va_list args;
 	unsigned int x;
-	
+
 	if (n == 0)
 	{
 		return (0);
 	}
-	
+
 	va_start(args, n);
 
 	for (x = 0; x < n; x++)
