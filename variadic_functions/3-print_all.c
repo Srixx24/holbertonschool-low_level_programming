b132bcb34244ce45b4d21ac41ae289c8badd4a96
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-10-29 19:42:06 -0700

    Compiler edit

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * print_all - function that prints anything
+ *
+ * @format: format specifier
+ *
+ * return: anything
+ */
+void print_all(const char * const format, ...)
+{
+	char c;
+	int i;
+	float f;
+	char *s;
+	int index = 0;
+	va_list args;
+
+	va_start(args, format);
+	
+	while (format[index] != '\0')
+	{
+		c = format[index];
+
+		if (c == 'c')
+		{
+			i = va_arg(args, int);
+			printf("%c", i);
+		}
+		else if (c == 'i')
+		{
+			i = va_arg(args, int);
+			printf("%d", i);
+		}
+		else if (c == 'f')
+		{
+			f = va_arg(args, double);
+			printf("%f", f);
+		}
+		else if (c == 's')
+		{
+			s = va_arg(arg, char *);
+			printf("%s", s);
+		}
+	}
+
+	va_end(arg);
+	
+	printf("\n");
+}
