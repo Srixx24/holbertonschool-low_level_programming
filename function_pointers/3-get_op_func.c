f0fdcbd08f341ae8ea7c2192283a006ff0847ac8
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-10-30 08:01:07 -0700

    Compiler edits

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -22,12 +22,15 @@ int (*get_op_func(char *s))(int, int)
 
 	int i = 0;
 
-	if (strcmp(s, ops[i].op) == 0)
+	while(i < 5)
 	{
-		return (ops[i].f);
-	}
+		if (strcmp(s, ops[i].op) == 0)
+		{
+			return (ops[i].f);
+		}
 
-	i++;
+		i++;
+	}
 
 	return (0);
 }
