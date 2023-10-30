029e2a8b1e1e9a6675d4f841dd42dcb2a240f05b
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-10-30 06:28:15 -0700

    Compiler edits

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -1,13 +1,21 @@
 #include "3-calc.h"
 #include <stdlib.h>
 #include <stdio.h>
+/**
+ * op_add - adding
+ * @a: first value
+ * @b: second value
+ *
+ * return: result
+ */
 
-int op_add (int a, int b)
+
+int op_add(int a, int b)
 {
-	return (a+ b);
+	return (a + b);
 }
 
-int op_sub (int a, int b)
+int op_sub(int a, int b)
 {
 	return (a - b);
 }
