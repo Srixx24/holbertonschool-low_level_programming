9245b580559feef8d5c010f6c7e3c4509529b20d
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-10-30 06:16:50 -0700

    first push

---
+++ function_pointers/3-main.c
@@ -0,0 +1,32 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - main fuction
+ *
+ * @argc: argument counter
+ * @argv: argument vector
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int (*oprt)(int, int);
+
+	if (argc)
+	{
+		boop
+	}
+
+	oprt = get_op_func(argv[2]);
+
+	if (!oprt)
+	{
+		boop
+	}
+
+	oprt(atoi(argv[1]), atoi(argv[3];
+	printf("%d\n");
+	
+	return (0);
