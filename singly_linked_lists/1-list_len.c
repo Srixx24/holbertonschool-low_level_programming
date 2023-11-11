9764e6f3092b20df1dd71d7cb6c6230fdfcf207c
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-11-11 10:41:36 -0800

    first push

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * list_len - function that returns the number
+ * of elements in a linked list_t list
+ *
+ * @h: 
+ *
+ * Return: count
+ */
+size_t list_len(const list_t *h)
+{
+	size_t count = 0;
+
+	if (h == NULL)
+	{
+		return count;
+	}
+	else
+	{
+		while(h != NULL)
+		{
+			count++;
+			h = h->next;
+		}
+	}
+
+	return count;
+}
