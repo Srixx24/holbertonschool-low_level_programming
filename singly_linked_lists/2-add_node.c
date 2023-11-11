07ff251a8e0566535340e41705231c59da9487c3
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-11-11 13:07:02 -0800

    compiler edits

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -14,12 +14,12 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new;
+	list_s *new;
 	
 	if (str == NULL)
 		return NULL;
 
-	new = malloc(sizeof(list_t));
+	new = malloc(sizeof(list_s));
 	if (new == NULL)
 		return (NULL);
 
