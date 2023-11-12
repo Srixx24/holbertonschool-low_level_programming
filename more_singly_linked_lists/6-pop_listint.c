3f2547c91c474bc4ce8ccebfac0fe85d721d0fc5
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-11-12 08:04:23 -0800

    Compiler edits

---
+++ more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * pop_listint - function that deletes the head node of a listint_t
+ * linked list, and returns the head node’s data (n).
+ *
+ * @head: head
+ *
+ * Return: head
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *temp;
+	int cur;
+
+	if (*head == NULL)
+		return (0);
+
+	temp = *head;
+	cur = (*head)->x;
+	*head = (*head)->next;
+
+	return (cur);
+}
