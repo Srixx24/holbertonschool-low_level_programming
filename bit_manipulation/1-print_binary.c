764dd0f129b45f3bc9070b189d5010b6ceb77b85
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-11-12 15:10:50 -0800

    Compiler edits

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -15,6 +15,11 @@ void print_binary(unsigned long int n)
 	unsigned long int bit = 1ul << (sizeof(n) * 8 - 1);
 	int extra = 0;
 
+	if (n == 0)
+	{
+		printf("0");
+		return;
+	}
 	while (bit > 0)
 	{
 		if (n & bit)
