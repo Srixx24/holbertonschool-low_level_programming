f10cf9016ef3611ac1fd6088cabdb9d514e7e443
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-11-11 22:39:13 -0800

    compiler edits

---
+++ more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_listint2 - function that frees a listint_t list
+ *
+ * @head: head
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *cur;
+
+	while (head != NULL)
+	{
+		cur = head;
+		head = head->next;
+		free(cur);
+	}
+	else
+	{
+		return (NULL)
+	}
+}
