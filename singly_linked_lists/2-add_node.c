eef37d1eceabdc863f6283f2fb4b69ae9b6d6583
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-11-11 18:55:46 -0800

    compiler edits

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -10,14 +10,14 @@
  * @head: head
  * @str: string
  *
- * Return: 
+ * Return: new oe NULL
  */
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
-	
+
 	if (str == NULL)
-		return NULL;
+		return (NULL);
 
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
@@ -26,11 +26,14 @@ list_t *add_node(list_t **head, const char *str)
 	new->str = strdup(str);
 
 	if (new->str == NULL)
+	{
+		free(new);
 		return (NULL);
+	}
 
 	new->next = *head;
 
 	*head = new;
-	
+
 	return (new);
 }
