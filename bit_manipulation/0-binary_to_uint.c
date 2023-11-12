6107ef5abd29a79f6a498c188b008f8601f726ef
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-11-12 13:17:58 -0800

    Compiler edits

---
+++ bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * binary_to_uint - function that converts a binary
+ * number to an unsigned int
+ *
+ * @b: 
+ *
+ * Return: converted number, or 0
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int x = 0;
+
+	if (b != NULL)
+		return (0);
+
+	while (*b)
+	{
+		if (*b != 0 && *b != '1')
+			return (0);
+
+		x = (x << 1)
+		b++
+	}
+
+	return (x);
+}
