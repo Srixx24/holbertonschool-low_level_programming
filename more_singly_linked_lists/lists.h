c31f4ad296f02644163da7b0aa61b33d771f10e7
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-11-11 20:41:33 -0800

    compiler edits

--- more_singly_linked_lists/lists.h
+++ more_singly_linked_lists/lists.h
@@ -6,10 +6,10 @@
  * @x: int
  * @next: next node
  */
-typedef struct listint_s
+typedef struct list_t
 {
 	int x;
-	struct listint_s *next;
+	struct list_t *next;
 } list_t;
 
 size_t print_listint(const listint_t *h);
