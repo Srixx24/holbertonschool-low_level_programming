1886330c8b29cc94fe4955542d177f6b6cb551b6
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-11-12 09:23:24 -0800

    Compiler edits

---
+++ more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - function that returns the nth node
+ * of a listint_t linked list
+ *
+ * @head: head
+ * @index: index
+ *
+ * Return: node or NULL
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *node;
+	
+	for (node = 0; node < index; node++)
+	{
+		if (head == NULL)
+			return (NULL);
+
+		head = head->next
+	}
+}
