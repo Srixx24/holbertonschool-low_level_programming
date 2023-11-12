d1e95e79c0f4c930836e6ac569d702a8f98cfe2b
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-11-11 19:14:04 -0800

    compiler edits

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -10,7 +10,7 @@
  * @head: head
  * @str: string
  *
- * Return:
+ * Return: new or NULL
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
@@ -28,7 +28,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	if (new->str == NULL)
 	{
 		free(new);
-		return NULL;
+		return (NULL);
 	}
 
 	new->next = *head;
---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * free_list - function that frees a list_t list
+ *
+ * @head: head
+ *
+ * Return: free
+ */
+void free_list(list_t *head)
+{
+
