0e7d1db4a93a9f0d0243cc12ea8dfb9c13fed9d5
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-11-12 07:37:09 -0800

    Compiler edits

--- more_singly_linked_lists/5-free_listint2.c
+++ more_singly_linked_lists/5-free_listint2.c
@@ -10,28 +10,19 @@
 void free_listint2(listint_t **head)
 {
 	listint_t *cur;
-	listint_t *next;
 	listint_t *temp;
-	int count = 0;
 
+	if (head == NULL)
+		return;
 
 	cur = *head;
 
 	while (cur != NULL)
 	{
-		if (cur->x < 0)
-		{
-			next = cur->next;
-			free(cur);
-			cur = *head;
-		}
-		else
-		{
-			temp = cur;
-			cur = cur->next;
-			free(cur);
-		}
-		count++;
+		temp = cur;
+		cur = cur->next;
+		free(temp);
+
 	}
 
 	*head = NULL;
