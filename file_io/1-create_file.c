95b1af6657b9b909666aebaa4c2866dedb75e142
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-11-13 12:32:56 -0800

    Complier edits

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <string.h>
 #include "main.h"
 
 /**
@@ -12,4 +17,24 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	boop
+	int file;
+	ssize_t bw;
+
+	if (filename == NULL)
+		return (-1);
+
+	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC);
+
+	if (file == -1)
+		return (-1);
+	bw = write(file, text_content);
+
+	if (bw == -1)
+	{
+		close(file);
+		return (-1);
+	}
+
+	close(file);
+	return (1);
+}
