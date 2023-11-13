b654161ac0e6236e058b04ca1b66ab4793976d43
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-11-12 20:51:24 -0800

    Compiler edits

--- bit_manipulation/5-flip_bits.c
+++ bit_manipulation/5-flip_bits.c
@@ -13,12 +13,12 @@
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned long int x = n ^m;
+	unsigned long int x = n ^ m;
 	unsigned long int b = 0;
 
 	while (x > 0)
 	{
-		b = (x & 1);
+		b += (x & 1);
 		x >>= 1;
 	}
 
