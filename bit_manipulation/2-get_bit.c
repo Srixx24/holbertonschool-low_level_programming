56efea5f6377f145967881123a4a92fabb7fd184
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-11-12 18:24:44 -0800

    Compiler edits

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -13,8 +13,8 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	if (index >= sizeof(n) *8)
+	if (index >= sizeof(n) * 8)
 		return (-1);
 
-	return (n >>index);
+	return (n >> index & 1);
 }
