5f98c3acea8eda11080c927d6da8479f5f42106b
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-11-12 20:39:23 -0800

    Compiler edits

--- bit_manipulation/4-clear_bit.c
+++ bit_manipulation/4-clear_bit.c
@@ -14,10 +14,10 @@
 int clear_bit(unsigned long int *n, unsigned int index)
 {
 	if (index < sizeof(unsigned long int) * 8)
-		return (-1);
+		return (1);
 
 	if (*n & (1UL << index))
 		*n ^= (1UL << index);
 
-	return (1);
+	return (-1);
 }
