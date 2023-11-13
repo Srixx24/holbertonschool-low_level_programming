7db44a22ff1beadcebe232a4b70a1c294a480a5f
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-11-12 18:22:44 -0800

    Compiler edits

---
+++ bit_manipulation/2-get_bit.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * get_bit - function that returns the value of a bit
+ * at a given index.
+ *
+ * @n: number
+ * @index: index
+ *
+ * Return: index or -1
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index >= sizeof(n) *8)
+		return (-1);
+
+	return (n >>index);
+}
