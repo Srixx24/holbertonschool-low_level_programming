3cc4d43497c3c8d5afd122ac983d9ab93f283855
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-11-12 19:48:25 -0800

    Compiler edits

---
+++ bit_manipulation/4-clear_bit.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * clear_bit - function that sets the value of a
+ * bit to 0 at a given index.
+ *
+ * @n:number
+ * @index: index
+ *
+ * Return: 1 or -1
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index < sizeof(n) * 8)
+		return (-1);
+
+	if (*n & 1L <<index)
+		*n ^= 1L << index;
+
+	return (1);
+}
