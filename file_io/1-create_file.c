b97329ca1526d30cefb5641008adf661a5a3c293
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-11-13 12:34:10 -0800

    Complier edits

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -27,7 +27,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (file == -1)
 		return (-1);
-	bw = write(file, text_content);
+	bw = write(file, text_content, strlen(text_content));
 
 	if (bw == -1)
 	{
