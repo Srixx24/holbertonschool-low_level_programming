fd54231051db27d8b8d29d69bd33185ef4076209
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-11-12 20:48:14 -0800

    Compiler edits

---
+++ bit_manipulation/5-flip_bits.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * flip_bits - function that returns the number of bits you
+ * would need to flip to get from one number to another
+ *
+ * @n: number
+ * @m: flip number
+ *
+ * Return: number of bits
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int x = n ^m;
+	unsigned long int b = 0;
+
+	while (x > 0)
+	{
+		b = (x & 1);
+		x >> 1;
+	}
+
+	return (b);
+}
