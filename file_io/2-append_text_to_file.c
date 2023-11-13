c4df99e1bbf2bbdd2d77ef7a5d6197762608b3e4
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-11-13 12:52:45 -0800

    Complier edits

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
 #include "main.h"
 
 /**
@@ -12,4 +15,25 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	boop
+	int file;
+	ssize_t bw;
+
+	if (file == NULL)
+		return (-1);
+
+	file = open(filname, O_WRONLY |O_APPEND);
+
+	if (file == -1)
+		return (-1);
+
+	bw = write(file, text_content, strlen(text_content));
+
+	if (bw == -1)
+	{
+		close(file);
+		return (-1);
+	}
+
+	close(file);
+	return (1);
+}
