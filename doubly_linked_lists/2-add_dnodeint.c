9f286c00533882457d9b99e8fdb1246075ed61c9
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-11-15 20:41:01 -0800

    compiler edits

---
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * add_dnodeint - function that adds a new node at
+ * the beginning of a dlistint_t list.
+ *
+ * @head: head
+ * @n: number
+ *
+ * Return: new or NULL
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new = malloc(sizeof(dlistint_t));
+
+	if (new == NULL)
+		return (NULL)
+	
+	new->str = n;
+	new->next = *head;
+	*head = new;
+
+	return (new);
+}
