919df23a620e0ef0060f80841105674b3926d14a
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-11-15 21:02:43 -0800

    compiler edits

---
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * add_dnodeint_end - function that adds a new node at the
+ * end of a dlistint_t list.
+ *
+ * @head: head
+ * @n: number
+ *
+ * Return: new or NULL
+ */
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new = malloc(sizeof(dlistint_t));
+	dlistint_t *cur = *head;
+
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+	new->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new;
+	}
+	else
+	{
+		while (cur->next != NULL)
+		{
+			cur = cur->next;
+		}
+	}
+
+	return (new);
+}
