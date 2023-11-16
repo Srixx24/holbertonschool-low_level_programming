1c85883e053de1d1be2927c163107d6ec3f65477
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-11-15 20:25:15 -0800

    First push

---
+++ doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * dlistint_len - function that returns the number of 
+ * elements in a linked dlistint_t list.
+ *
+ * @h: double list
+ *
+ * Return: elements in list
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		count++
+		h = h->next;
+	}
+
+	return (count);
+}
