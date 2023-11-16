f96f87a3d251e14b903f14a641a76dd45d83c4fa
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-11-15 20:42:57 -0800

    compiler edits

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -16,11 +16,11 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	dlistint_t *new = malloc(sizeof(dlistint_t));
 
 	if (new == NULL)
-		return (NULL)
+		return (NULL);
 	
-	new->str = n;
-	new->next = *head;
+	n->str = n;
+	n->next = *head;
 	*head = new;
 
-	return (new);
+	return (n);
 }
