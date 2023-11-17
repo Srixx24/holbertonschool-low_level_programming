12ec27fbe71f07cb1af539201daa1f809e92eb1a
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-11-16 20:33:16 -0800

    First push

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * free_dlistint - function that frees a dlistint_t list.
+ *
+ * @head: head
+ *
+ * Return: free
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *cur;
+
+	while (head)
+	{
+		cur = head;
+		new->prev = cur;
+		head = head->next;
+		free(cur);
+	}
+}
