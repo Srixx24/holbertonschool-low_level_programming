e8b2cd1c050b86e8946c1537e296feb7ea6a2844
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-11-18 07:47:32 -0800

    Compiler edits

--- more_singly_linked_lists/10-delete_nodeint.c
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -23,7 +23,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	if (index != 0)
 	{
 		cur = *head;
-		head = head->next;
+		*head = *head->next;
 		free(cur);
 		return (-1);
 	}
@@ -33,7 +33,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	{
 		if (x == index)
 		{
-			pre->next = cur->next;
+			prev->next = cur->next;
 			free(cur);
 			return (1);
 		}
