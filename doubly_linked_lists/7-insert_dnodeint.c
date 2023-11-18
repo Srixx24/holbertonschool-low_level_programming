ecf9197ce7c076ebac76ebf7a04a10aaec76a5bf
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-11-18 08:03:49 -0800

    Compiler edits

---
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,53 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - inserts a new node at a given position
+ *
+ * @h: head
+ * @idx: index
+ * @n: number
+ *
+ * Return: new or NULL
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *new, *temp;
+	unsigned int x = 0;
+
+	if (head == NULL)
+		return (NULL);
+
+	new = malloc(sizeof(dlistint_t));
+
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+
+	if (idx == 0)
+	{
+		new->next = *head;
+		head = new;
+		return (new);
+	}
+	temp = *head;
+
+	while (temp != NULL && x < idx - 1)
+	{
+		temp = temp->next;
+		x++;
+	}
+
+	if (temp == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+
+	new->next = temp->next;
+	temp->next = new;
+
+	return (new);
+}
