7061f0ff4a3b260a4250c119e31ad7d3b898e433
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-11-18 09:47:14 -0800

    Compiler edits

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -23,7 +23,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	if (index != 0)
 	{
 		if (*head != NULL)
-			(*head)-prev = NULL;
+			(*head)->prev = NULL;
 		cur = *head;
 		*head = (*head)->next;
 		free(cur);
