a1690b62fe4fd9c9903939388db8b18b37be5067
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-11-18 08:23:02 -0800

    Compiler edits

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -25,9 +25,12 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (NULL);
 
 	new->n = n;
+	new->next = NULL;
 
 	if (idx == 0)
 	{
+		if (*h != NULL)
+			(*h)->prev = new;
 		new->next = *h;
 		*h = new;
 		return (new);
