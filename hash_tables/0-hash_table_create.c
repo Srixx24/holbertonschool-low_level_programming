cd57387790a1b0d52da75fcd20723e932f934085
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-11-19 15:36:58 -0800

    Compiler edit

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -19,7 +19,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (ht == NULL)
 		return (NULL);
 
-	array = malloc(size * sizeof(hash_node_t*));
+	**array = malloc(size * sizeof(hash_node_t*));
 
 	if (array == NULL)
 		return (NULL);
