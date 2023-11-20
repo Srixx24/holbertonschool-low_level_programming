d1d094b929471be6389d6d6e82720139030a5138
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-11-19 17:58:15 -0800

    Compiler edit

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -37,6 +37,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		}
 	}
 
+	new->value = strdup(value);
+	new->key = strdup(key);
 	new->next = ht->array[idx];
 	ht->array[idx] = new;
 
