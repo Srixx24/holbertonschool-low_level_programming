c8fd2137a8f03f2b6f740bb37a13b57e6394145d
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-11-19 16:05:10 -0800

    Compiler edit

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -12,4 +12,8 @@
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	boop
+	unsigned long int hvalue = hash_djb2(key);
+	unsigned long int index = hvalue % size;
+
+	return (index)
+}
