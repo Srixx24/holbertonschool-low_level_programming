099bdf5160ff24d34bdbda9037636ca26eb9aa32
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-12-17 09:48:29 -0800

    linear

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -6,7 +6,7 @@
  * @array: array
  * @size: size
  * @value: value
- * Return: 0
+ * Return: -1 if not found
  */
 int linear_search(int *array, size_t size, int value)
 {
@@ -17,11 +17,11 @@ int linear_search(int *array, size_t size, int value)
 
 	for (x = 0; x < size; x++)
 	{
-		printf("%d\n", array[x]);
+		printf("Value checked array[%s] = [%d]\n", array, array[x]);
 
 		if (array[x] == value)
 			return (x);
 	}
-	
+
 	return (-1);
 }
