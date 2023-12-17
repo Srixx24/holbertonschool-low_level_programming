5635d8e85f453f9f60311e1b24c7d3b59f2ce3a8
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-12-17 11:19:33 -0800

    binary

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -30,7 +30,7 @@ int binary_search(int *array, size_t size, int value)
 
 		if (array[mid] < value)
 			return (mid);
-		else if (array[mid] < value)
+		else if (array[mid] > value)
 			low = mid + 1;
 		else
 			hi = mid - 1;
